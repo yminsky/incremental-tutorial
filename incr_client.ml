d3e6f4db0f47bd079d9e79ff3481d9a3e9c0bff7
Yaron Minsky <yminsky@janestreet.com>
2017-08-30 08:17:14 -0400

    Generalized incremental viewer

--- incr_client.ml
+++ incr_client.ml
@@ -2,7 +2,7 @@ open! Core
 open! Async
 open! Import
 
-let run ~host ~port =
+let connect_and_view ~host ~port ~view ~print =
   Command_common.connect_and_process_events ~host ~port (fun pipe ->
     let state_v = Incr.Var.create State.empty in
     let pipe_finished = 
@@ -14,24 +14,34 @@ let run ~host ~port =
       )
     in
     let state = Incr.Var.watch state_v in
-    let failed_checks = Incr_view.failed_checks state in
-    let obs = Incr.observe failed_checks in
-    let print x =
-      print_s [%sexp (x : (Host.Name.t * Check.Name.t, string) Map.Poly.t)]
-    in
+    let view = view state in
+    let obs = Incr.observe view in
+    Incr.stabilize ();
+    let viewer = Viewer.create ~print ~init:(Incr.Observer.value_exn obs) in
     Incr.Observer.on_update_exn obs ~f:(fun update ->
-      let (_:int) = Core.Sys.command "clear" in
       match update with
-      | Initialized v | Changed (_,v) -> print v
-      | Invalidated -> printf "------- STATE INVALIDATED -----------\n%!"
+      | Initialized v | Changed (_,v) -> Viewer.update viewer v
+      | Invalidated -> ()
     );
     pipe_finished
   )
 
+let print_sexp sexp_of x =
+  print_s (sexp_of x)
+
+let incr_command ~summary ~view ~print =
+  Command.async' ~summary
+    (let open Command.Let_syntax in
+     [%map_open
+       let (host, port) = Command_common.host_and_port in
+       (fun () -> 
+          connect_and_view ~host ~port ~view ~print)])
+    
 let command =
-  let open Command.Let_syntax in
-  Command.async'
-    ~summary:"Start client"
-    [%map_open
-      let (host, port) = Command_common.host_and_port in
-      (fun () -> run ~host ~port)]
+  Command.group ~summary:"Various incremental views"
+    [ "failed", 
+      incr_command
+        ~summary:"Failed view" 
+        ~view:Incr_view.failed_checks
+        ~print:(print_sexp [%sexp_of: (Host.Name.t * Check.Name.t, string) Map.Poly.t])
+    ]
