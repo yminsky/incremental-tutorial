476ad8e72404aab3dc1d1a6783550455fd1199b6
Yaron Minsky <yminsky@janestreet.com>
2017-09-05 08:02:28 -0400

    added compute-time tracking to Viewer

--- shared/viewer.mli
+++ shared/viewer.mli
@@ -5,4 +5,8 @@ type 'display_state t
    
 val create : print:('display_state -> unit) -> 'display_state t
 
+(** To register a new value to be printed *)
 val update : 'display_state t -> 'display_state -> unit
+
+(** Used to register the computation phase *)
+val compute : _ t -> (unit -> 'a) -> 'a
