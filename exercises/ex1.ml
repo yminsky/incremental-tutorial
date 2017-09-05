19668ef02994c1bf31f5dedb648312f18dcca9a0
Yaron Minsky <yminsky@janestreet.com>
2017-09-04 20:59:50 -0400

    cleaned up ex1

--- exercises/ex1.ml
+++ exercises/ex1.ml
@@ -11,71 +11,81 @@
 open! Core
 open! Import
 open! Incr.Let_syntax
-   
+
 type what_operation = Multiply | Add
 
-let simple_f what x y z =
-  match what with
-  | Multiply -> x * y * z
-  | Add -> x + y
-;;
+module Simple = struct
 
-let simple_run () =
-  let x = ref 50 in
-  let y = ref 120 in
-  let z = ref 250 in
-  let what = ref Add in
-  (* This is an all-at-once computation *)
-  let compute () =
-    printf "%d\n" (simple_f !what !x !y !z)
-  in
-  compute ();
-  x := 150;
-  y := 90;
-  compute ();
-  what := Multiply;
-  compute ();
-;;
+  let f what x y z =
+    match what with
+    | Multiply -> x * y * z
+    | Add -> x + y
+  ;;
 
-(* These are the functions you need to implement incrementally. *)
+  let run () =
+    let x = ref 50 in
+    let y = ref 120 in
+    let z = ref 250 in
+    let what = ref Add in
+    (* This is an all-at-once computation *)
+    let compute () =
+      printf "%d\n" (f !what !x !y !z)
+    in
+    compute ();
+    x := 150;
+    y := 90;
+    compute ();
+    what := Multiply;
+    compute ();
+  ;;
+
+end
+
+module Incremental = struct
+
+  (* These are the functions you need to implement incrementally. *)
 
 
-(* [f] is supposed to take in incrementals and return
-   incrementals. Here, we want to use bind on the [what] argument to
-   choose which of the two computations to do.*)
-let f (what:what_operation Incr.t) (x:int Incr.t) (y: int Incr.t) (z:int Incr.t)
-  : int Incr.t 
-  =
-  ignore (x,y,z,what);
-  failwith "implement me"
-;;    
+  (* [f] is supposed to take in incrementals and return
+     incrementals. Here, we want to use bind on the [what] argument to
+     choose which of the two computations to do.*)
+  let f (what:what_operation Incr.t) (x:int Incr.t) (y: int Incr.t) (z:int Incr.t)
+    : int Incr.t 
+    =
+    ignore (x,y,z,what);
+    failwith "implement me"
+  ;;    
 
-(* The structure of [run[ should follow that of [simple_run] above
-   closely, except:
+  (* The structure of [run[ should follow that of [simple_run] above
+     closely, except:
 
-   - OCaml references should be replcaed with [Incr.Var.t]'s
-   - [f] should be called just once
-   - An observer should be created based on the result of [f]
-   - [Incr.stabilize] needs to be called as part of [compute]
-   - [compute] should then get its value using [Incr.Observer.value_exn].
-*)
-let run () : unit =
-  failwith "implement me"
-;;
+     - OCaml references should be replcaed with [Incr.Var.t]'s
+     - [f] should be called just once
+     - An observer should be created based on the result of [f]
+     - [Incr.stabilize] needs to be called as part of [compute]
+     - [compute] should then get its value using [Incr.Observer.value_exn].
+  *)
+  let run () : unit =
+    failwith "implement me"
+  ;;
+
+end
 
 
 (* From here on is the declaration of the command-line interface,
    which you can mostly ignore for the purposes of the tutorial. *)
-let simple_command =
-  Command.basic' ~summary:"all-at-once implementation"
-    (Command.Param.return (fun () -> simple_run ()))
 
-let incr_command =
-  Command.basic' ~summary:"incremental implementation"
-    (Command.Param.return (fun () -> run ()))
+let make_command ~summary run =
+  Command.basic' ~summary (Command.Param.return run)
+
+let simple_command =
+  make_command ~summary:"all-at-once implementation" Simple.run
+
+let incremental_command =
+  make_command ~summary:"incremental implementation" Incremental.run
 
 let command =
   Command.group ~summary:"Exercise 1"
     [ "simple"      , simple_command
-    ; "incremental" , incr_command
+    ; "incremental" , incremental_command
     ]
