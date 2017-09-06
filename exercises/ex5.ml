ede926290347dba04d9cdbc40d6ef6e424f2aea6
Yaron Minsky <yminsky@janestreet.com>
2017-09-07 00:27:54 +0100

    made ex4 and ex5

--- exercises/ex5.ml
+++ exercises/ex5.ml
@@ -1,124 +1,93 @@
-(* Similarly to exercise 3, we want to display information about failed checks.
-
-   This time we'd like to display the descriptions of all tests that
-   are currently failing.
-
-   The tricky bit however comes from how we want to represent this:
-
-   You could imagine having a 
-   {v 
-      string Check.Name.t Host.Name.t Map.t Incr.t
-   v}
-
-   to represent this, i.e. the outer map (keyed by hostname)
-   contains a map from check name to description.
-
-   But for the purpose of this exercise, let's represent this as a
-   /flat/ map of type 
-
-   {v 
-      string (Check_name.t * Host.Name.t) Map.t Incr.t
-   v}
-
-*)
+(* Compute the nth stalest checks *)
 
 open! Core
 open! Async
 open! Import
 
-let print_failure_descriptions c =
-  print_s [%sexp (c : (Host.Name.t * Check.Name.t, string) Map.Poly.t)]
+(* Note that this is sorted first by time, then by host name *)
+module Time_and_host = struct
+  include Tuple.Make       (Time) (Host.Name)
+  include Tuple.Comparable (Time) (Host.Name)
+  include Tuple.Sexpable   (Time) (Host.Name)
+end
+
+type result = Check.Name.t Time_and_host.Map.t
+[@@deriving sexp]
+
+let print_result x =
+  print_s [%sexp (x : result)]
+
+(** Returns the single stalest check from the map of checks *)
+let stalest_check (checks : State.checks) = 
+  match Map.to_alist checks with
+  | [] -> None
+  | (check,(time,_)) :: rest ->
+    let (time,check) =
+      List.fold rest ~init:(time,check)
+        ~f:(fun ((oldest_time,_) as acc) (check,(time,_)) ->
+          if time < oldest_time then (time,check) else acc)
+    in
+    Some (time,check)
 
 module Simple = struct
 
-  let failed_checks (state : State.t) () =
-    Map.fold ~init:Map.Poly.empty state.hosts ~f:(fun ~key:host_info ~data:(_,checks) acc ->
-        Map.fold checks ~init:acc ~f:(fun ~key:check_name ~data:(_,outcome) acc ->
-            match (outcome : Protocol.Check.Outcome.t option) with
-            | None | Some Passed -> acc
-            | Some (Failed description) ->
-              Map.add acc ~key:(host_info, check_name) ~data:description
-          ))
-        
-  let process_events (events : Event.t Pipe.Reader.t) =
-    let viewer = Viewer.create ~print:print_failure_descriptions in
+  let hosts_by_staleness (s:State.t) : result =
+    List.filter_map (Map.to_alist s.hosts) ~f:(fun (host,(_,checks)) ->
+      match stalest_check checks with
+      | None -> None
+      | Some (time,check) ->
+        Some ((time,host),check))
+    |> List.fold ~init:Time_and_host.Map.empty
+         ~f:(fun acc (key,data) -> Map.add acc ~key ~data)
+         
+  let stalest (s:State.t) ~max_count : result =
+    let result = hosts_by_staleness s in
+    if Map.length result <= max_count then result
+    else
+      Map.to_sequence result
+      |> (fun s -> Sequence.take s max_count)
+      |> Sequence.fold ~init:Time_and_host.Map.empty ~f:(fun acc (key,data) ->
+        Map.add acc ~key ~data)
+
+  let process_events (events : Event.t Pipe.Reader.t) ~(max_count:int) =
+    let viewer = Viewer.create ~print:print_result in
     let state = ref State.empty in
     Pipe.iter' events ~f:(fun eventq ->
-      Queue.iter eventq ~f:(fun event ->
-            state := State.update !state event);
-      let update = Viewer.compute viewer (failed_checks !state) in
-      Viewer.update viewer update;
+      state := Queue.fold eventq ~init:!state ~f:State.update;
+      let stalest = Viewer.compute viewer (fun () -> stalest !state ~max_count) in
+      Viewer.update viewer stalest;
       return ()
     )
 end
 
-
 module Incremental = struct
-  open! Incr.Let_syntax
 
-  (* First, let's write a helper function that applies [f]
-     incrementally to [inc] but keeps track of the input and output of
-     the last time [f] ran. You will need to use a [ref] here. *)
-  let diff_map (inc : 'a Incr.t) ~(f : old:('a * 'b) option -> 'a -> 'b) : 'b Incr.t =
-    ignore (inc, f);
-    failwith "Implement me"
+  let stalest (s:State.t Incr.t) ~(max_count:int) : result Incr.t =
+    ignore s; ignore max_count;
+    failwith "Implement me!"
 
-  (* Next, let's write the function to flatten [State.t Incr.t] into a
-     map keyed by [Host.Name.t * Check.Name.t]. (Incr_map has a
-     [flatten] function built in, but we want to ignore that and write
-     this from first principals.)
+  let process_events (events : Event.t Pipe.Reader.t) ~(max_count:int) =
+    ignore events; ignore max_count;
+    failwith "Implement me!"
 
-     The basic idea is to use [diff_map] to find all the keys that
-     were added, removed or changed between the old and current input,
-     and apply those changes to the old output to get the new output.
-
-     Check out [Map.symmetric diff] for an efficient way of
-     calculating diffs between maps.  *)
-  let flatten_maps
-      (mm : State.t Incr.t)
-    : (Host.Name.t * Check.Name.t,Time.t * Check.Outcome.t option,_) Map.t Incr.t
-    =
-    ignore mm;
-    failwith "implement me"
-
-
-  (* Use [flatten_maps] here to compute the final result. *)
-  let failed_checks (s:State.t Incr.t) : (Host.Name.t * Check.Name.t, string) Map.Poly.t Incr.t =
-    ignore s;
-    failwith "implement me"
-
-  (* The structure of process_events will be fairly similar to the
-     corresponding function in exercise 3 *)
-  let process_events (events : Event.t Pipe.Reader.t) : unit Deferred.t =
-    ignore events;
-    failwith "implement me"
-      
 end
 
 
-
-(* Command line setup *)
-
-let build_command ~summary process_events =
-  Command.async' ~summary
-    (let open Command.Let_syntax in
-     [%map_open
-       let (host, port) = Command_common.host_and_port_param in
-       fun () -> 
-         Command_common.connect_and_process_events 
-           process_events ~host ~port
-     ])
-
-let simple = 
-  build_command ~summary:"Simple, all-at-once implementation"
-    Simple.process_events
-
-let incremental =
-  build_command ~summary:"Incremental implementation"
-    Incremental.process_events
-
 let command =
+  let cmd summary process_events =
+    Command.async' ~summary
+      (let open Command.Let_syntax in
+       [%map_open
+         let (host, port) = Command_common.host_and_port_param 
+         and max_count = flag "-max-count" (optional_with_default 10 int)
+                        ~doc:"The number of hosts to show"
+         in
+         fun () -> 
+           Command_common.connect_and_process_events 
+             (fun pipe -> process_events pipe ~max_count) ~host ~port
+       ])
+  in
   Command.group ~summary:"Exercise 5"
-    [ "simple", simple
-    ; "incremental", incremental
+    [ "simple"      , cmd "all-at-once implementation" Simple.process_events
+    ; "incremental" , cmd "incremental implementation" Incremental.process_events
     ]
