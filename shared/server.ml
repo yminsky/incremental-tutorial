811d249784c7a37d12137d97c001325df35f52e1
Yaron Minsky <yminsky@janestreet.com>
2017-09-07 01:01:18 +0100

    minor tweaks

--- shared/server.ml
+++ shared/server.ml
@@ -14,13 +14,16 @@ let events_impl ~(make_stream:unit -> (unit -> (Time.t * Event.t)) Staged.t) ~st
     let started_at = Time_ns.now () in
     let events_written = ref 0 in
     begin
-      if stats
-      then Clock.every (Time.Span.of_sec 1.)  ~stop:(Pipe.closed w)
-            (fun () ->
-              let sec_since_start = Time_ns.diff (Time_ns.now ()) started_at |> Time_ns.Span.to_sec in
-              log (sprintf "Total-events: %d     Events/sec %.3f%!"
-                           !events_written
-                           (Float.of_int !events_written /. sec_since_start)))
+      if stats then 
+        Clock.every (Time.Span.of_sec 1.)  ~stop:(Pipe.closed w)
+          (fun () ->
+             let sec_since_start = 
+               Time_ns.diff (Time_ns.now ()) started_at
+               |> Time_ns.Span.to_sec 
+             in
+             log (sprintf "Total-events: %d     Events/sec %.3f%!"
+                    !events_written
+                    (Float.of_int !events_written /. sec_since_start)))
     end;
     let rec write () =
       let (time,event) = next () in
@@ -45,29 +48,29 @@ let implementations ~make_stream ~stats =
 let serve ~make_stream ~port ~stats =
   let%bind _tcp_server =
     Tcp.Server.create
-     ~on_handler_error:`Ignore
-     (Tcp.on_port port)
-     (fun addr r w ->
-       Rpc.Connection.server_with_close r w
-         ~connection_state:(fun _ -> addr)
-         ~on_handshake_error:`Raise
-         ~implementations:(implementations ~make_stream ~stats)
-     )
+      ~on_handler_error:`Ignore
+      (Tcp.on_port port)
+      (fun addr r w ->
+         Rpc.Connection.server_with_close r w
+           ~connection_state:(fun _ -> addr)
+           ~on_handshake_error:`Raise
+           ~implementations:(implementations ~make_stream ~stats)
+      )
   in
   Log.Global.info "Server started";
   Deferred.unit
-  
+
 let go ~port ~num_hosts ~time_scale ~stats =
   let make_stream =
     let time = Time.now () in
     let rs = Random.State.make_self_init () in
     (fun () ->
-      Generator.stream
-        (Random.State.copy rs)
-        time
-        ~num_hosts
-        ~pct_initially_active:0.20
-        ~time_scale
+       Generator.stream
+         (Random.State.copy rs)
+         time
+         ~num_hosts
+         ~pct_initially_active:0.20
+         ~time_scale
     )
   in
   let%bind () = serve ~make_stream ~port ~stats in
@@ -81,14 +84,14 @@ let command =
       let port = 
         flag "port" (required int) ~doc:"PORT port to listen for clients" 
       and num_hosts =
-        flag "hosts" (optional_with_default 100 int)
+        flag "hosts" (optional_with_default 1000 int)
           ~doc:"N number of hosts to simulate"
       and time_scale =
         flag "time-scale" (optional_with_default (Time.Span.of_sec 0.01) time_span)
           ~doc:"maximum time to the next event"
       and stats =
         flag "print-stats" no_arg ~doc:" Print stats about event generation per client."
-     in
-     fun () ->
-       go ~port ~num_hosts ~time_scale ~stats
+      in
+      fun () ->
+        go ~port ~num_hosts ~time_scale ~stats
     ]
